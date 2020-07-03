282e9953d0e346ef99cfd5aefe4e2c27c0beb3d7
Steph <steph.valais@gmx.com>
2020-07-03 10:30:44 +1000

    ye??

--- Assets/Scripts/Customisation/Get.cs
+++ Assets/Scripts/Customisation/Get.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Get : MonoBehaviour
+namespace Customisation
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Get : MonoBehaviour
     {
-        
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
--- Assets/Scripts/Customisation/Set.cs
+++ Assets/Scripts/Customisation/Set.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class Set : MonoBehaviour
+namespace Customisation
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Set : MonoBehaviour
     {
-        
+        #region Variables
+        [Header("Character Name")]
+        public string characterName;
+        [Header("Character Class")]
+        public CharacterClass characterClass = CharacterClass.Fighter;
+        [Header("Dropdown Menu")]
+        public Dropdown classDropdown; //needed?
+        [Header("Texture Lists")]
+        public List<Texture2D> skin = new List<Texture2D>();
+        public List<Texture2D> eyes, hair, mouth, armour, clothes = new List<Texture2D>();
+        [Header("Index")]
+        public int skinIndex;
+        public int eyesIndex, hairIndex, mouthIndex, armourIndex, clothesIndex;
+        [Header("Renderer")]
+        public Renderer characterRenderer;
+        [Header("Max textures per type")]
+        public int skinMax;
+        public int eyesMax, hairMax, mouthMax, armourMax, clothesMax;
+        #endregion
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+}
+public enum CharacterClass
+{
+    Fighter,
+    Rogue,
+    Witch
 }
